552f07e132bfb8519f7455a44ec23fbc79d2aa87
Anthony Ortiz <ortiz.anthony@gmail.com>
2019-10-02 08:28:54 -0400

    Update sql_queries.py
    adding pk, not null constraints; on-conflict directives for insert statements

--- Data-Modeling/Project1/sql_queries.py
+++ Data-Modeling/Project1/sql_queries.py
@@ -8,39 +8,37 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
-songplay_id varchar, 
-start_time timestamp, 
-user_id varchar, 
-level varchar,
-song_id varchar, 
-artist_id varchar,
-session_id varchar, 
-location varchar, 
-user_agent varchar
-  );
-""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users(user_id int not null primary key, first_name varchar, last_name varchar, gender varchar, level varchar);""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users(user_id varchar, first_name varchar, last_name varchar, gender varchar, level varchar);""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(song_id varchar not null primary key, title varchar, artist_id varchar, year int, duration int);""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(song_id varchar, title varchar, artist_id varchar, year varchar, duration int);""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id varchar not null primary key, name varchar, location varchar, latitude float, longitude float);""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id varchar, name varchar, location varchar, latitude varchar, longitude varchar);""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time varchar not null primary key, hour int, day int, week int, month int, year int, weekday int);""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time varchar, hour varchar, day varchar, week varchar, month varchar, year varchar, weekday varchar);""")
+songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays (\
+                            songplay_id serial primary key, \
+                            start_time varchar not null, \
+                            user_id int not null, \
+                            level varchar , \
+                            song_id varchar, \
+                            artist_id varchar, \
+                            session_id varchar not null, \
+                            location varchar, \
+                            user_agent varchar)")
+
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays(songplay_id,start_time,user_id,level,song_id,artist_id,session_id,location,user_agent) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""")
+songplay_table_insert = ("""INSERT INTO songplays(start_time,user_id,level,song_id,artist_id,session_id,location,user_agent) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) on conflict (songplay_id) do nothing""")
 
-user_table_insert = ("""INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s,%s,%s,%s,%s)""")
+user_table_insert = ("""INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s,%s,%s,%s,%s) on conflict (user_id) do update set first_name=excluded.first_name,last_name=excluded.last_name,gender=excluded.gender,level=excluded.level""")
 
-song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES(%s,%s,%s,%s,%s)""")
+song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES(%s,%s,%s,%s,%s) on conflict (song_id) do nothing""")
 
-artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude) VALUES(%s,%s,%s,%s,%s)
-""")
+artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude) VALUES(%s,%s,%s,%s,%s) on conflict (artist_id) do nothing""")
 
-time_table_insert = ("""INSERT INTO TIME(start_time, hour, day, week, month, year, weekday) VALUES(%s,%s,%s,%s,%s,%s,%s)""")
+time_table_insert = ("""INSERT INTO TIME(start_time, hour, day, week, month, year, weekday) VALUES(%s,%s,%s,%s,%s,%s,%s) on conflict (start_time) do nothing""")
 
 # FIND SONGS
 #find the song ID and artist ID based on the title, artist name, and duration of a song.
